4c67f82709181f88a310212997405fc2ca7d05cb
alghareebm <alghareebm@gmail.com>
2014-03-10 04:31:03 -0400

    Delete unneeded comments

--- Monopoly/src/edu/ncsu/monopoly/Card.java
+++ Monopoly/src/edu/ncsu/monopoly/Card.java
@@ -4,8 +4,7 @@ public abstract class Card {
 
     public static int TYPE_CHANCE = 1;
     public static int TYPE_CC = 2;
-    //hello nobody
-    //Hi
+   
 
     public abstract String getLabel();
     public abstract void applyAction();
