9664f683894ec9a589cab9c5a0004bbf95653917
alghareebm <alghareebm@gmail.com>
2014-03-11 01:15:29 -0400

    Testing project 2 dummy

---
+++ Project2Dummy/src/edu/towson/cis/cosc603/alghareeb/project2/Project2Servlet.java
@@ -0,0 +1,13 @@
+package edu.towson.cis.cosc603.alghareeb.project2;
+
+import java.io.IOException;
+import javax.servlet.http.*;
+
+@SuppressWarnings("serial")
+public class Project2Servlet extends HttpServlet {
+	public void doGet(HttpServletRequest req, HttpServletResponse resp)
+			throws IOException {
+		resp.setContentType("text/plain");
+		resp.getWriter().println("Hello, world");
+	}
+}
